2d46a47c74958f7ac6e4594bcb675313b46574bf
fastily <fastily@users.noreply.github.com>
2019-07-20 16:04:01 -0700

    minor edit - housekeeping, remove comments

--- faxmachine-project/faxes/views.py
+++ faxmachine-project/faxes/views.py
@@ -6,10 +6,7 @@ from django.shortcuts import render, redirect
 from django.conf import settings
 
 from twilio.rest import Client
-# from twilio.twiml.fax_response import FaxResponse
 
-# import firebase_admin
-# from firebase_admin import credentials
 from firebase_admin import storage
 
 from sendgrid import SendGridAPIClient
@@ -23,9 +20,6 @@ def send(request):
     """Sends a fax"""
     recipient = request.POST.get("recipient")
 
-    # if (not len(firebase_admin._apps)):
-    # firebase_admin.initialize_app(credentials.Certificate(settings.FIREBASE_KEYS), {'storageBucket': settings.FIREBASE_STORAGE_BUCKET})
-
     blob = storage.bucket().blob(str(uuid.uuid4()) + ".pdf")
     blob.upload_from_file(request.FILES.get("document"))
     blob.make_public()
@@ -33,13 +27,11 @@ def send(request):
     fax = Client(settings.ACCOUNT_SID, settings.AUTH_TOKEN).fax.faxes.create(from_=settings.MY_NUMBER, to="+1" + recipient.replace("-", ""), media_url=blob.media_link)
     # print(fax.sid)
 
-    message = Mail(
+    response = SendGridAPIClient(settings.SENDGRID_API_KEY).send(Mail(
         from_email=From(settings.FROM_EMAIL_ADDRESS, settings.SENDER_NAME),
         to_emails=settings.TO_EMAIL_ADDRESS,
         subject=f'Confirming fax to {recipient} was sent',
-        html_content=f'Confirming fax to {recipient} was sent.\n\nA copy of the sent file is available <a href="{blob.media_link}">here</a>.')
-
-    response = SendGridAPIClient(settings.SENDGRID_API_KEY).send(message)
+        html_content=f'Confirming fax to {recipient} was sent.\n\nA copy of the sent file is available <a href="{blob.media_link}">here</a>.'))
     # print(response.status_code)
     # print(response.body)
     # print(response.headers)
