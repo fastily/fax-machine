01816ed81982ad92e36706114dd2fdee9f581b47
fastily <fastily@users.noreply.github.com>
2019-07-19 17:28:31 -0700

    bug fix - only initialize firebase admin once

--- faxmachine-project/faxes/apps.py
+++ faxmachine-project/faxes/apps.py
@@ -1,5 +1,12 @@
+"""config for faxes app"""
 from django.apps import AppConfig
+from django.conf import settings
 
+import firebase_admin
+from firebase_admin import credentials
 
 class FaxesConfig(AppConfig):
     name = 'faxes'
+
+    def ready(self):
+        firebase_admin.initialize_app(credentials.Certificate(settings.FIREBASE_KEYS), {'storageBucket': settings.FIREBASE_STORAGE_BUCKET})
--- faxmachine-project/faxes/views.py
+++ faxmachine-project/faxes/views.py
@@ -6,10 +6,10 @@ from django.shortcuts import render, redirect
 from django.conf import settings
 
 from twilio.rest import Client
-from twilio.twiml.fax_response import FaxResponse
+# from twilio.twiml.fax_response import FaxResponse
 
-import firebase_admin
-from firebase_admin import credentials
+# import firebase_admin
+# from firebase_admin import credentials
 from firebase_admin import storage
 
 from sendgrid import SendGridAPIClient
@@ -23,14 +23,15 @@ def send(request):
     """Sends a fax"""
     recipient = request.POST.get("recipient")
 
-    firebase_admin.initialize_app(credentials.Certificate(settings.FIREBASE_KEYS), {'storageBucket': settings.FIREBASE_STORAGE_BUCKET})
+    # if (not len(firebase_admin._apps)):
+    # firebase_admin.initialize_app(credentials.Certificate(settings.FIREBASE_KEYS), {'storageBucket': settings.FIREBASE_STORAGE_BUCKET})
 
     blob = storage.bucket().blob(str(uuid.uuid4()) + ".pdf")
     blob.upload_from_file(request.FILES.get("document"))
     blob.make_public()
 
     fax = Client(settings.ACCOUNT_SID, settings.AUTH_TOKEN).fax.faxes.create(from_=settings.MY_NUMBER, to="+1" + recipient.replace("-", ""), media_url=blob.media_link)
-    print(fax.sid)
+    # print(fax.sid)
 
     message = Mail(
         from_email=From(settings.FROM_EMAIL_ADDRESS, settings.SENDER_NAME),
